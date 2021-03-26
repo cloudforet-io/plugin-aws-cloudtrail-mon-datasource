ed377623c2dda8bbd982a071f3148f3139744c10
Dong Yoo Kang(Xellos00) <xellos.kang@gmail.com>
2021-03-26 11:42:12 +0900

    Update for followings to update cloud_trail
    - data_source.py
    - data_source_manager.py
    - data_source_service.py
    - data_source_response_model.py

--- src/spaceone/monitoring/model/data_source_response_model.py
+++ src/spaceone/monitoring/model/data_source_response_model.py
@@ -2,11 +2,23 @@ from schematics.models import Model
 from schematics.types import ListType, DictType, StringType
 from schematics.types.compound import ModelType
 
-__all__ = ['PluginVerifyResponseModel']
+__all__ = ['PluginInitResponse']
 
 _SUPPORTED_RESOURCE_TYPE = [
     'inventory.Server',
     'inventory.CloudService'
+]
+
+_REQUIRED_KEYS = ['reference.resource_id']
+
+_REFERENCE_KEYS = [
+    {
+      'resource_type': 'inventory.Server',
+      'reference_key': 'data.stackdriver'
+    }, {
+      'resource_type': 'inventory.CloudService',
+      'reference_key': 'data.stackdriver'
+    }
 ]
 
 
@@ -15,24 +27,11 @@ class ReferenceKeyModel(Model):
     reference_key = StringType(required=True)
 
 
-class PluginOptionsModel(Model):
+class PluginMetadata(Model):
     supported_resource_type = ListType(StringType, default=_SUPPORTED_RESOURCE_TYPE)
-    reference_keys = ListType(ModelType(ReferenceKeyModel),
-                              default=[{
-                                  'resource_type': 'inventory.Server',
-                                  'reference_key': 'reference.resource_id'
-                              }, {
-                                  'resource_type': 'inventory.CloudService',
-                                  'reference_key': 'reference.resource_id'
-                              }])
+    required_keys = ListType(StringType, default=_REQUIRED_KEYS)
+    reference_keys = ListType(ModelType(ReferenceKeyModel), default=_REFERENCE_KEYS)
 
 
-class PluginVerifyModel(Model):
-    options = ModelType(PluginOptionsModel, default=PluginOptionsModel)
-
-
-class PluginVerifyResponseModel(Model):
-    resource_type = StringType(required=True, default='monitoring.DataSource')
-    actions = ListType(DictType(StringType))
-    result = ModelType(PluginVerifyModel, required=True, default=PluginVerifyModel)
-
+class PluginInitResponse(Model):
+    _metadata = ModelType(PluginMetadata, default=PluginMetadata, serialized_name='metadata')
